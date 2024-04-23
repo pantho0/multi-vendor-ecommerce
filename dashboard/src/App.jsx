fa03081059c4757b9aa712db57dfce573e845e2b
pantho0 <panthomashreky6@gmail.com>
2024-04-23 16:34:35 +0600

    intialized with dependencies and folder structure

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-2xl text-center bg-teal-400 text-white">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
