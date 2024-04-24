af5566895808e1f2d54c9b0cf85b728d8635774e
pantho0 <panthomashreky6@gmail.com>
2024-04-24 17:57:15 +0600

    state applied to get user infos for redux

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,17 +1,16 @@
-import React, { lazy,Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/Router";
-const App = lazy(()=>import('./App'))
+import App from "./App";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <RouterProvider router={router}>
-    <Suspense>
-      <App />
-    </Suspense>
+      <App/>
   </RouterProvider>
 );
 
--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 
 const Register = () => {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const inputHandle = (e) =>{
+    setState({
+        ...state,
+        [e.target.name] : e.target.value
+    })
+  }
+
+  const submit = (e) =>{
+    e.preventDefault()
+    console.log(state);
+  }
+
   return (
     <div className="min-w-full min-h-screen bg-[#cdcae9] flex justify-center items-center">
       <div className="w-[350px] text-white p-2">
@@ -14,10 +32,12 @@ const Register = () => {
           <p className="text-sm mb-3 font-medium text-center">
             Please register your account
           </p>
-          <form>
+          <form onSubmit={submit}>
             <div className="flex flex-col w-full gap-1 mb-3">
               <label htmlFor="name">Name</label>
               <input
+                onChange={inputHandle}
+                value={state.name}
                 className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md"
                 type="text"
                 name="name"
@@ -29,6 +49,8 @@ const Register = () => {
             <div className="flex flex-col w-full gap-1 mb-3">
               <label htmlFor="email">Email</label>
               <input
+                onChange={inputHandle}
+                value={state.email}
                 className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md"
                 type="email"
                 name="email"
@@ -40,6 +62,8 @@ const Register = () => {
             <div className="flex flex-col w-full gap-1 mb-3">
               <label htmlFor="password">Password</label>
               <input
+                onChange={inputHandle}
+                value={state.password}
                 className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md"
                 type="password"
                 name="password"
@@ -89,7 +113,6 @@ const Register = () => {
                 </span>
               </div>
             </div>
-
           </form>
         </div>
       </div>
