a13edf7ea19d86aca773ff6ea9428e65a3abe73c
pantho0 <panthomashreky6@gmail.com>
2024-04-29 17:03:05 +0600

    order page searchbar and data load button add

--- dashboard/src/router/routes/adminRoutes.js
+++ dashboard/src/router/routes/adminRoutes.js
@@ -1,10 +1,16 @@
 import AdminDashboard from "../../views/admin/AdminDashboard";
+import Orders from "../../views/admin/Orders";
 
 
 export const adminRoutes = [
     {
         path : 'admin/dashboard',
         element : <AdminDashboard/>,
+        role : 'admin'
+    },
+    {
+        path : 'admin/dashboard/orders',
+        element : <Orders/>,
         role : 'admin'
     }
 ]
