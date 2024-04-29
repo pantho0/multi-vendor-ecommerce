849126f4b8224b0aef0708f7ba3f016c6ec9337d
pantho0 <panthomashreky6@gmail.com>
2024-04-29 13:39:21 +0600

    responsive functionalites added to chart for small devices

--- dashboard/src/views/admin/AdminDashboard.jsx
+++ dashboard/src/views/admin/AdminDashboard.jsx
@@ -45,7 +45,27 @@ const AdminDashboard = () => {
             },
             legend : {
                 position : 'top'
-            }
+            },
+            responsive : [
+                {
+                    breakpoint : 565,
+                    yaxis :{
+                        categories : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug','Sep', 'Oct', 'Nov', 'Dec']
+        
+                    },
+                    options : {
+                        plotOptions : {
+                            bar : {
+                                horizontal : true
+                            }
+                        },
+                        chart :{
+                            height : '550px'
+                        }
+                    }
+                }
+            ]
+            
         }
     }
 
