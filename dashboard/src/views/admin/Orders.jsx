68fb44a1f2d17c8a18667e94a34cdad17b2506f8
pantho0 <panthomashreky6@gmail.com>
2024-04-29 17:22:00 +0600

    order page custom view table added

--- dashboard/src/views/admin/Orders.jsx
+++ dashboard/src/views/admin/Orders.jsx
@@ -1,25 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { BsArrowBarDown } from "react-icons/bs";
+import { Link } from 'react-router-dom';
 
 const Orders = () => {
-    const [currentPage, setCurrentPage] = useState(1)
-    const [searchValue, setSearchValue] = useState('')
-    const [perPage, setPerPage] = useState(5)
-    return (
-        <div className='px-2 lg:px-7 pt-5'>
-            <div className='w-full p-4 bg-[#6a5fdf] rounded-md '>
-
-                <div className='flex justify-between items-center'>
-                    <select onChange={(e)=>setPerPage(parseInt(e.target.value))} className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]'>
-                        <option value="5">5</option>
-                        <option value="10">10</option>
-                        <option value="20">20</option>
-                    </select>
-                    <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-transparent border border-slate-700 rounded-md text-[#d0d2d6]' type="text" placeholder='Search' />
-                </div>
-
-            </div>
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchValue, setSearchValue] = useState("");
+  const [perPage, setPerPage] = useState(5);
+  return (
+    <div className="px-2 lg:px-7 pt-5">
+      <div className="w-full p-4 bg-[#6a5fdf] rounded-md ">
+        <div className="flex justify-between items-center">
+          <select
+            onChange={(e) => setPerPage(parseInt(e.target.value))}
+            className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
+          >
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+          </select>
+          <input
+            className="px-4 py-2 focus:border-indigo-500 outline-none bg-transparent border border-slate-700 rounded-md text-[#d0d2d6]"
+            type="text"
+            placeholder="Search"
+          />
         </div>
-    );
+
+        <div className="relative mt-5 overflow-x-auto ">
+          <div className="w-full tex-sm text-left text-[#d0d2d6]">
+            <div className="text-sm text-[#d0d2d6] uppercase border-b border-slate-700">
+              <div className="flex justify-between items-center">
+                <div className="py-3 w-[25%] font-bold">Order Id</div>
+                <div className="py-3 w-[13%] font-bold">Price</div>
+                <div className="py-3 w-[18%] font-bold">Payment Status</div>
+                <div className="py-3 w-[18%] font-bold">Order Status</div>
+                <div className="py-3 w-[18%] font-bold">Action</div>
+                <div className="py-3 w-[8%] font-bold">
+                  <BsArrowBarDown />
+                </div>
+              </div>
+            </div>
+
+            <div className="text-[#d0d2d6]">
+              <div className="flex justify-between items-start border-b border-slate-700">
+                <div className="py-3 w-[25%] font-medium whitespace-nowrap">#445454</div>
+                <div className="py-3 w-[13%] font-medium whitespace-nowrap">$545</div>
+                <div className="py-3 w-[18%] font-medium whitespace-nowrap">Pending</div>
+                <div className="py-3 w-[18%] font-medium whitespace-nowrap">Pending</div>
+                <div className="py-3 w-[18%] font-medium whitespace-nowrap">
+                    <Link>View All</Link>
+                </div>
+                <div className="py-3 w-[8%] font-bold">
+                  <BsArrowBarDown />
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
