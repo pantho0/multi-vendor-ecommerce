e71ca8316973d8de56a243b0a23d7a9631b58fd2
pantho0 <panthomashreky6@gmail.com>
2024-04-30 17:57:43 +0600

    seller status updating button added

--- dashboard/src/views/admin/SellerDetails.jsx
+++ dashboard/src/views/admin/SellerDetails.jsx
@@ -41,7 +41,46 @@ const SellerDetails = () => {
                             </div>
                         </div>
                     </div>
+                    <div className='w-4/12'>
+                        <div className='px-0 md:px-5 py-2'>
+                            <div className='py-2  text-lg'>
+                                <h2>Address</h2>
+                            </div>
+                            <div className='flex justify-between text-sm flex-col gap-2 p-4 bg-[#9D9CED] rounded-md'>
+                                <div className='flex gap-2 font-bold text-[#000000]'>
+                                    <span>Shop Name : </span>
+                                    <span>Easy Shop</span>
+                                </div>
+                                <div className='flex gap-2 font-bold text-[#000000]'>
+                                    <span>Division : </span>
+                                    <span>Dhaka</span>
+                                </div>
+                                <div className='flex gap-2 font-bold text-[#000000]'>
+                                    <span>District : </span>
+                                    <span>Dhaka</span>
+                                </div>
+                                <div className='flex gap-2 font-bold text-[#000000]'>
+                                    <span>State : </span>
+                                    <span>Paltan</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
+                </div>
+                <div>
+                    <form>
+                        <div className='flex gap-4 py-3'>
+                            <select className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' name="" id="">
+                                <option value="">--Select Option--</option>
+                                <option value="active">Active</option>
+                                <option value="deactive">Deactive</option>
+                            </select>
+                            <button className="bg-red-500 w-[170px] hover:shadow-red-500/50 hover:shadow-md text-white rouded-md px-7 py-2">
+                      Submit
+                    </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
