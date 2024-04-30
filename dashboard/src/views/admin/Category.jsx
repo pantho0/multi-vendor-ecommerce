c87ed729ec2023353b50dfb5920a2c627627a5a3
pantho0 <panthomashreky6@gmail.com>
2024-04-30 13:20:14 +0600

    category adding input design applied

--- dashboard/src/views/admin/Category.jsx
+++ dashboard/src/views/admin/Category.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Pagination from "../Pagination";
+import img1 from "../../assets/images/category/1.jpg";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
 const Category = () => {
+  const [show, setShow] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchValue, setSearchValue] = useState("");
   const [perPage, setPerPage] = useState(5);
+
   return (
     <div className="px-2 lg:px-7 pt-5">
       <div className="flex flex-wrap w-full">
@@ -25,117 +31,88 @@ const Category = () => {
               />
             </div>
             <div className="relative overflow-x-auto">
-          <table className="w-full text-sm text-[#d0d2d6] text-left">
-            <thead className="text-sm text-[#d0d2d6] uppercase border-b border-slate-700">
-            <tr>
-              <th scope="col" className="py-3 px-4">
-                No
-              </th>
-              <th scope="col" className="py-3 px-4">
-                Image
-              </th>
-              <th scope="col" className="py-3 px-4">
-                Name
-              </th>
-              <th scope="col" className="py-3 px-4">
-                Action
-              </th>
-            </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                #345454
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                $454
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-               Pending
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                pending
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                view
-                </td>
-              </tr>
-              <tr>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                #345454
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                $454
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-               Pending
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                pending
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                view
-                </td>
-              </tr>
-              <tr>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                #345454
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                $454
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-               Pending
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                pending
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                view
-                </td>
-              </tr>
-              <tr>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                #345454
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                $454
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-               Pending
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                pending
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                view
-                </td>
-              </tr>
-              <tr>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                #345454
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                $454
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-               Pending
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                pending
-                </td>
-                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap">
-                view
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+              <table className="w-full text-sm text-[#d0d2d6] text-left">
+                <thead className="text-sm text-[#d0d2d6] uppercase border-b border-slate-700">
+                  <tr>
+                    <th scope="col" className="py-3 px-4">
+                      No
+                    </th>
+                    <th scope="col" className="py-3 px-4">
+                      Image
+                    </th>
+                    <th scope="col" className="py-3 px-4">
+                      Name
+                    </th>
+                    <th scope="col" className="py-3 px-4">
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {[1, 2, 3, 4, 5].map((d, i) => (
+                    <tr key={i}>
+                      <td
+                        scope="row"
+                        className="py-1 px-4 font-medium whitespace-nowrap"
+                      >
+                        {d}
+                      </td>
+                      <td
+                        scope="row"
+                        className="py-1 px-4 font-medium whitespace-nowrap"
+                      >
+                        <img className="w-[45px] h-[45px]" src={img1} alt="" />
+                      </td>
+                      <td
+                        scope="row"
+                        className="py-1 px-4 font-medium whitespace-nowrap"
+                      >
+                        T-Shirt
+                      </td>
+                      <td
+                        scope="row"
+                        className="py-1 px-4 font-medium whitespace-nowrap"
+                      >
+                        <div className="flex justify-start items-center gap-4">
+                          <Link className="p-[6px] bg-yellow-500 rounded hover:shadow-lg hover:shadow-yellow-500/50">
+                            <FaEdit />
+                          </Link>
+                          <Link className="p-[6px] bg-red-500 rounded hover:shadow-lg hover:shadow-red-500/50">
+                            <FaTrash />
+                          </Link>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+
+            <div className="w-full flex justify-end mt-4 bottom-4 right-4">
+              <Pagination
+                pageNumber={currentPage}
+                setPageNumber={setCurrentPage}
+                totalItem={50}
+                perPage={perPage}
+                showItem={3}
+              />
+            </div>
           </div>
         </div>
 
-        <div className="w-full lg:w-5/12">
-          <div className="w-full p-4 bg-[#6a5fdf] rounded-md"></div>
+        <div className={`w-[320px] lg:w-5/12 translate-x-100 lg:relative lg:right-0 fixed ${show ? 'right-0' : '-right-[340px]'} z-20 top-0 transition-all duration-500`}>
+          <div className="w-full pl-5">
+                <div className="bg-[#6a5fdf] h-screen lg:h-auto px-3 py-2 lg:rouded-md text-[#d0d2d6] font-semibold text-xl mb-4 w-full text-center">
+                    <h1>Add Category</h1>
+                    <form>
+                        <div className="flex flex-col w-full gap-1 mb-3">
+                        <label className="text-sm text-left font-normal" htmlFor="name">Category Name</label>
+                        <input className="px-4 py-2 focus:border-indigo-500 outline-none bg-white border border-slate-700 rounded-md text-[#d0d2d6]" type="text" id="name" name="category_name" placeholder="Category Name" />
+                        </div>
+                    </form>
+
+                </div>
+          </div>
         </div>
       </div>
     </div>
