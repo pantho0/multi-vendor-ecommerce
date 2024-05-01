3400a02f7418dfcbec9bf1b94375a89604786c1b
pantho0 <panthomashreky6@gmail.com>
2024-05-01 16:01:18 +0600

    added button to manage chat bar at small screen

--- dashboard/src/views/admin/ChatSeller.jsx
+++ dashboard/src/views/admin/ChatSeller.jsx
@@ -1,29 +1,32 @@
 import React, { useState } from "react";
 import { IoIosCloseCircle } from "react-icons/io";
 import img1 from "../../assets/images/admin.jpg";
+import img2 from "../../assets/images/demo.jpg";
+import { FaList } from 'react-icons/fa';
 const ChatSeller = () => {
   const [show, setShow] = useState(false);
+  const sellerId = 65;
   return (
     <div className="px-2 lg:px-7 py-5">
       <div className="w-full bg-[#6A5FDF] px-4 py-4 rounded-md h-[calc(100vh-140px)]">
         <div className="flex w-full h-full relative">
           <div
             className={`w-[280px] h-full absolute z-10 ${
-              show ? "-left-[16px]" : ""
+              show ? "-left-[16px]" : "-left-[336px]"
             } md:left-0 md:relative transition-all`}
           >
             <div className="w-full h-[calc(100vh-177px)] bg-[#9e97e9] md:bg-transparent overflow-y-auto">
               <div className="flex tex-xl justify-between items-center p-4 md:p-0 md:px-3 md:pb-3 text-white">
                 <h2>Sellers</h2>
                 <span
-                  onClick={() => setShow(true)}
+                  onClick={() => setShow(!show)}
                   className="block cursor-pointer md:hidden"
                 >
                   <IoIosCloseCircle />
                 </span>
               </div>
               <div
-                className={`h-[60px] flex justify-start gap-2 items-center text-white px-2 py-2 rounded-sm cursor-pointer`}
+                className={`h-[60px] flex justify-start gap-2 items-center text-white px-2 py-2 rounded-md cursor-pointer bg-[#808CF0]`}
               >
                 <div className="relative">
                   <img
@@ -76,7 +79,31 @@ const ChatSeller = () => {
             </div>
             
           </div>
-        </div>
+          <div className="w-full md:w-[calc(100%-200px)] md:pl-4">
+              <div className="flex justify-between items-center">
+                  {
+                    sellerId && <div className="flex justify-start items-center gap-3">
+                      <div className="relative">
+                      <img
+                    className="w-[45px] h-[45px] rounded-full border-green-500 border-2 max-w-[45px] p-[2px]"
+                    src={img2}
+                    alt=""
+                  />
+                  <div className="w-[10px] h-[10px] rounded-full bg-green-500 absolute right-0 bottom-0"></div>
+                      </div>
+                    </div>
+                  }
+                <div onClick={()=>setShow(!show)} className="w-[35px] flex md:hidden h-[35px] rounded-sm bg-blue-600 shadow-lg hover:shadow-blue-500/50 justify-center cursor-pointer items-center text-white">
+                    <span><FaList/></span>
+                </div>
+
+              </div>
+            </div>
+      </div>
+
+            
+
+
       </div>
     </div>
   );
