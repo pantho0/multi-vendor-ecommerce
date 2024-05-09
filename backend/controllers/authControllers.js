10979946054f0f1d21856095fb01b117e280c310
pantho0 <panthomashreky6@gmail.com>
2024-05-09 21:39:09 +0600

    seller chat id & token generation function added and throw it via cookie

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,8 +1,9 @@
 const adminModel = require("../models/adminModel");
 const sellerModel = require("../models/sellerModel");
+const sellerCustomerModel = require("../models/chat/sellerCustomerModel");
 const { createToken } = require("../utilities/generateToken");
 const { responseReturn } = require("../utilities/response");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const authControllers = {
   auth: async (req, res) => {
@@ -15,18 +16,18 @@ const authControllers = {
         const match = await bcrypt.compare(password, admin.password);
         if (match) {
           const token = await createToken({
-            id: admin._id,
-            role: admin.role
+            id: admin.id,
+            role: admin.role,
           });
-          res.cookie('accessToken', token, {
-            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+          res.cookie("accessToken", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
           });
           responseReturn(res, 200, { token, message: "Login Success" });
         } else {
-          responseReturn(res, 404, { error: 'Wrong Password' });
+          responseReturn(res, 404, { error: "Wrong Password" });
         }
       } else {
-        responseReturn(res, 404, { error: 'Email Not Found' });
+        responseReturn(res, 404, { error: "Email Not Found" });
       }
     } catch (error) {
       responseReturn(res, 500, { error: error.message });
@@ -34,40 +35,49 @@ const authControllers = {
   },
 
   seller_register: async (req, res) => {
-    const {email, name, password} = req.body;
+    const { email, name, password } = req.body;
     try {
-      const getUser = await sellerModel.findOne({email})
+      const getUser = await sellerModel.findOne({ email });
       if (getUser) {
-        responseReturn(res, 404, { error: 'Email Already Exist' });
-      }else{
+        responseReturn(res, 404, { error: "Email Already Exist" });
+      } else {
         const seller = await sellerModel.create({
           name,
           email,
-          password : await bcrypt.hash(password, 10),
-          method : 'manually',
-          shopInfo : {}
-        })
-        console.log(seller);
+          password: await bcrypt.hash(password, 10),
+          method: "manually",
+          shopInfo: {},
+        });
+        await sellerCustomerModel.create({
+          myId: seller.id,
+        });
+        const token = await createToken({
+          id: seller.id,
+          role: seller.role,
+        });
+        res.cookie("accessToken", token, {
+          expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        });
+        responseReturn(res, 201, { token, message: "Register Success" });
       }
     } catch (error) {
-      console.log(error);
+      responseReturn(res, 500, { error: "Internal Server Error" });
     }
-    
   },
 
   getUser: async (req, res) => {
     const { id, role } = req;
     try {
-      if (role === 'admin') {
+      if (role === "admin") {
         const user = await adminModel.findById(id);
         responseReturn(res, 200, { userInfo: user });
       } else {
-        console.log('seller info');
+        console.log("seller info");
       }
     } catch (error) {
       console.log(error.message);
     }
-  }
+  },
 };
 
 module.exports = authControllers;
