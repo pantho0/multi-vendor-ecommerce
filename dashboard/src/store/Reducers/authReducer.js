c36a632e41f7d777b76953765bc496fa0f21e33e
pantho0 <panthomashreky6@gmail.com>
2024-05-10 01:36:24 +0600

    login functionalities added and updated backend and state managed with redux

--- dashboard/src/store/Reducers/authReducer.js
+++ dashboard/src/store/Reducers/authReducer.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import api from "../../api/api";
+import {jwtDecode} from "jwt-decode"
 
 export const admin_login = createAsyncThunk(
     'auth/admin_login',
@@ -33,6 +34,7 @@ export const seller_register = createAsyncThunk(
     } 
 )
 
+//
 
 export const seller_login = createAsyncThunk(
     'auth/seller_login',
@@ -50,6 +52,37 @@ export const seller_login = createAsyncThunk(
     } 
 )
 
+//
+
+export const get_user_info = createAsyncThunk(
+    'auth/get_user_info',
+    async(_, {rejectWithValue, fulfillWithValue}) =>{
+        try{
+            const {data} = await api.get('/get-info',{withCredentials:true})
+            // console.log(data)
+            return fulfillWithValue(data)
+        }catch(error){
+            // console.log(error.response.data);
+            return rejectWithValue(error.response.data) 
+        }
+    } 
+)
+
+const returnRole = (token) =>{
+    if (token) {
+        const decodeToken = jwtDecode(token)
+        const expireTime = new Date(decodeToken.exp * 1000)
+        if (new Date() > expireTime) {
+            localStorage.removeItem('accessToken')
+            return ""
+        } else {
+            return decodeToken.role
+        }
+    } else {
+        return ''
+    }
+}
+
 
 
 
@@ -59,7 +92,9 @@ export const authReducer = createSlice({
         successMessage : "",
         errorMessage : "",
         loader : false,
-        userInfo : ""
+        userInfo : "",
+        role : returnRole(localStorage.getItem('accessToken')),
+        token : ""
     },
     reducers : {
         messageClear : (state,_)=>{
@@ -79,6 +114,8 @@ export const authReducer = createSlice({
         .addCase(admin_login.fulfilled, (state,{payload})=>{
             state.loader = false;
             state.successMessage = payload.message
+            state.token = payload.token
+            state.role = returnRole(payload.token)
         })
 
         //
@@ -93,6 +130,8 @@ export const authReducer = createSlice({
         .addCase(seller_register.fulfilled, (state,{payload})=>{
             state.loader = false;
             state.successMessage = payload.message
+            state.token = payload.token
+            state.role = returnRole(payload.token)
         })
 
         //
@@ -107,6 +146,13 @@ export const authReducer = createSlice({
         .addCase(seller_login.fulfilled, (state,{payload})=>{
             state.loader = false;
             state.successMessage = payload.message
+        })
+        //
+        .addCase(get_user_info.fulfilled, (state,{payload})=>{
+            state.loader = false;
+            state.userInfo = payload.userInfo
+            state.token = payload.token
+            state.role = returnRole(payload.token)
         })
     }
 })
